7b8f7d5dcf6149c0c27a89eb49f86cb98d8fb10d
Siddhant <siddhantbhalla28.sb@gmail.com>
2026-06-21 13:28:46 +0530

    add e2e navigation test for ivysaur

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -1,4 +1,4 @@
-const { test, describe, expect } = require("@playwright/test");
+import { test, describe, expect } from "@playwright/test";
 
 describe("Pokedex", () => {
   test("front page can be opened", async ({ page }) => {
@@ -9,5 +9,13 @@ describe("Pokedex", () => {
         "Pokémon and Pokémon character names are trademarks of Nintendo.",
       ),
     ).toBeVisible();
+  });
+
+  test("pokemon page can be navigated to", async ({ page }) => {
+    await page.goto("");
+    // Find the link or text for ivysaur and click it
+    await page.getByText("ivysaur").click();
+    // Check that the app navigated and displays the ability "chlorophyll"
+    await expect(page.getByText("chlorophyll")).toBeVisible();
   });
 });
